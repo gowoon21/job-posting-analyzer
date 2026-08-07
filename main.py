16b0f4b739b7032d6c46500d946917a9cbe3b240
gowoon21 <gownmea@naver.com>
2026-08-07 23:27:32 +0900

    feat: add basic job posting crawler

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from models import JobPosting
 from parser import parse_job_posting
+from crawler import get_job_info
 
 
 def main():
     posting = parse_job_posting()
 
     print(posting)
-    parse_job_posting()
 
 
 if __name__ == "__main__":
--- parser.py
+++ parser.py
@@ -1,17 +1,22 @@
 from models import JobPosting
+from crawler import get_job_info
 
 
 def parse_job_posting():
     print("채용공고를 불러옵니다.")
 
+    url = "https://openai.com/careers/ai-deployment-engineer-seoul-south-korea/"
+
+    job_title, location = get_job_info(url)
+
     return JobPosting(
         company_name= "OpenAI",                     #회사 이름
-        job_title= "Python Developer",              #직무
-        location= "Seoul",                          #지역
+        job_title= job_title,              #직무
+        location= location,                          #지역
         employment_type= "정규직",                   #고용 형태
-        experience= "신입",                          #경력
+        experience= "경력",                          #경력
         education= "학력 무관",                      #학력
         salary= "연봉 협의",                         #급여
-        tech_stacks=["Python", "FastAPI"],          #기술 스택
-        preferred_qualifications=["Git 사용 경험"]   #우대사항
+        tech_stacks=["Python"],                     #기술 스택
+        preferred_qualifications=[]                 #우대사항
     )
