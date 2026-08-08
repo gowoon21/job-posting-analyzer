299470aba6738903807831e8aa17ad239e96e8ff
gowoon21 <gownmea@naver.com>
2026-08-08 18:31:53 +0900

    fix: add error handling to crawler

--- src/crawler.py
+++ src/crawler.py
@@ -4,20 +4,31 @@ from bs4 import BeautifulSoup   #bs4 패키지 안에 있는 BeautifulSoup 사�
 def get_job_info(url):
     response = requests.get(url)
     # requests.get(url) : 해당 url에 get 요청을 보내는 코드
+    response.raise_for_status()
+    # 결과가 정상이 아니면 python에게 요청에 문제가 있다고 오류를 발생시키는 기능
 
     soup = BeautifulSoup(response.text, "html.parser")
     # response.text : 서버에서 받은 HTML 내용을 문자열로 꺼냄
     # BeautifulSoup(..., "html.parser") : 그 HTML을 Python이 다루기 쉽게 분석함
 
-    job_title = soup.find("h2").get_text(strip=True)
-    # HTML에서 첫 번째 <h2>를 찾아서 job_title에 저장
+    job_title_tag = soup.find("h2")
 
+    if job_title_tag:
+        job_title = job_title_tag.get_text(strip=True)
+    else:
+        job_title = "직무명 없음"
+
+    
     location_text = soup.find(string=lambda text: text and "Technical Success - Seoul, South Korea" in text)
     # 텍스트 중에 "Seoul"이 들어간 첫 번째 문자열을 찾는 방식
     # find_all() : 조건에 맞는 모든 요소를 찾음
     
-    location = location_text.split(" - ")[1]
-    # 문자열을 " - " 기준으로 나눔
+
+    if location_text:
+        location = location_text.split(" - ")[1]
+        # 문자열을 " - " 기준으로 나눔
+    else:
+        location = "장소명 없음"
     
     return job_title, location
 
